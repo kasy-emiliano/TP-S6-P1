e0438303a62281ba2544a53ea40d7929dffbe6de
kasy-emiliano <99735371+kasy-emiliano@users.noreply.github.com>
2023-05-08 23:11:12 +0300

    cooo

--- src/main/java/com/example/demo/model/Connect.java
+++ src/main/java/com/example/demo/model/Connect.java
@@ -10,7 +10,7 @@ import java.sql.DriverManager;
 public class Connect {
      public java.sql.Connection con() throws Exception{
         Class.forName("org.postgresql.Driver");
-        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/aticle","article","article");
+        return DriverManager.getConnection("jdbc:postgresql://containers-us-west-180.railway.app:6027/railway","postgres","WpZhHXX7Dl3KMFNqu9fu");
     }
     
 }
